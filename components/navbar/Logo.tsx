5b0bbcb96fac2ac44811cd56288e852e4c09b4a2
Murithi <kelvinmurithi@live.com>
2024-10-26 23:38:50 +0300

    Update Logo component

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Link from 'next/link';
+import { Button } from '../ui/button';
+import { VscCode } from 'react-icons/vsc';
 
 function Logo() {
   return (
-    <div>Logo</div>
-  )
+    <Button size='icon' asChild>
+      <Link href='/'>
+        <VscCode className='w-6 h-6' />
+      </Link>
+    </Button>
+  );
 }
 
-export default Logo
+export default Logo;
