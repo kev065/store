5d980e3e8b57fd76b9c05a1454c207b61983d284
Murithi <kelvinmurithi@live.com>
2024-11-09 21:06:52 +0300

    refactor actions.ts

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,16 +1,13 @@
 import db from '@/utils/db';
 
-export const fetchFeaturedProducts = async () => {
-  const products = await db.product.findMany({
-    where: {
-      featured: true,
-    },
-  });
-  return products;
-};
-
-export const fetchAllProducts = () => {
+export const fetchAllProducts = ({ search = '' }: { search: string }) => {
   return db.product.findMany({
+    where: {
+      OR: [
+        { name: { contains: search, mode: 'insensitive' } },
+        { company: { contains: search, mode: 'insensitive' } },
+      ],
+    },
     orderBy: {
       createdAt: 'desc',
     },
