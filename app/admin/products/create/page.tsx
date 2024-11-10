8c9022888ab3a9da1a7996a67cb14005a31e7014
Murithi <kelvinmurithi@live.com>
2024-11-10 18:22:16 +0300

    Add Admin Page components

---
+++ app/admin/products/create/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CreateProductPage() {
+  return (
+    <div>CreateProductPage</div>
+  )
+}
+
+export default CreateProductPage;
---
+++ app/admin/products/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProductsPage() {
+  return (
+    <div>Admin Products Page</div>
+  )
+}
+
+export default ProductsPage
