38fe9d0908638e749dfb4a2b620ec0a93bba05d6
Murithi <kelvinmurithi@live.com>
2024-11-11 20:07:00 +0300

    remove 'https' from hostname

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'https://oenqmnnczfppofsblbkk.supabase.co',
+        hostname: 'oenqmnnczfppofsblbkk.supabase.co',
       },
     ],
   },
