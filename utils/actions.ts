d8cc2254aa35d3ad331494c734ed7c9f0cd5d21f
Murithi <kelvinmurithi@live.com>
2024-11-11 16:16:26 +0300

    Update actions

--- utils/actions.ts
+++ utils/actions.ts
@@ -63,21 +63,13 @@ export const createProductAction = async (
   const user = await getAuthUser();
 
   try {
-    const name = formData.get('name') as string;
-    const company = formData.get('company') as string;
-    const price = Number(formData.get('price') as string);
-    const image = formData.get('image') as File;
-    const description = formData.get('description') as string;
-    const featured = Boolean(formData.get('featured') as string);
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = productSchema.parse(rawData);
 
     await db.product.create({
       data: {
-        name,
-        company,
-        price,
+        ...validatedFields,
         image: '/images/product-1.jpg',
-        description,
-        featured,
         clerkId: user.id,
       },
     });
