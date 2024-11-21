eea75b1330d7041b719c7c775f3ef495db606a54
Murithi <kelvinmurithi@live.com>
2024-11-21 19:50:04 +0300

    Moved the key prop to the <Button> element

--- app/admin/Sidebar.tsx
+++ app/admin/Sidebar.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { adminLinks } from '@/utils/links';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -12,13 +13,15 @@ function Sidebar() {
       {adminLinks.map((link) => {
         const isActivePage = pathname === link.href;
         const variant = isActivePage ? 'default' : 'ghost';
+
         return (
           <Button
+            key={link.href} // Move `key` here
             asChild
             className='w-full mb-2 capitalize font-normal justify-start'
             variant={variant}
           >
-            <Link key={link.href} href={link.href}>
+            <Link href={link.href}>
               {link.label}
             </Link>
           </Button>
@@ -27,4 +30,5 @@ function Sidebar() {
     </aside>
   );
 }
+
 export default Sidebar;
