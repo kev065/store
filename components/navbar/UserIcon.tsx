d8b3d1792affaa8f39c85581a34549df3d17befb
Murithi <kelvinmurithi@live.com>
2024-11-21 20:03:37 +0300

    Replaced <img> with <Image>
    - Added alt Attribute
    - Adjusted Dimensions

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,12 +1,24 @@
 import { LuUser2 } from 'react-icons/lu';
 import { currentUser } from '@clerk/nextjs/server';
+import Image from 'next/image';
+
 async function UserIcon() {
   const user = await currentUser();
   const profileImage = user?.imageUrl;
-  if (profileImage)
+
+  if (profileImage) {
     return (
-      <img src={profileImage} className='w-6 h-6 rounded-full object-cover' />
+      <Image
+        src={profileImage}
+        alt="User profile image"
+        width={24} // Match your 'w-6' class (6 x 4px)
+        height={24} // Match your 'h-6' class
+        className="rounded-full object-cover"
+      />
     );
-  return <LuUser2 className='w-6 h-6 bg-primary rounded-full text-white' />;
+  }
+
+  return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
+
 export default UserIcon;
